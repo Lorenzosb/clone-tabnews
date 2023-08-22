2133133e65efc24897d835a834d42d764b393d6b
LorenzX <83486486+Lorenzosb@users.noreply.github.com>
2023-08-22 01:06:52 +0000

    teste

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,14 @@
 function Home() {
-  return <h1>Oi meu amor, você poderia me ligar??</h1>;
+  return (
+    <>
+      <h1>EM DESENVOLVIMENTO</h1>
+      <style>
+        color: #2B44E7; text-align: center; font-family: Suez One; font-size:
+        21.86px; font-style: normal; font-weight: 400; line-height: 39.999px; /*
+        182.98% */
+      </style>
+    </>
+  );
 }
 
 export default Home;

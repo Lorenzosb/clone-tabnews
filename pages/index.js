f7f3631ee006afa326cf2867fe9cdc57520a01ab
LorenzX <83486486+Lorenzosb@users.noreply.github.com>
2023-10-24 21:46:50 +0000

    teste2

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 function Home() {
   return (
     <>
-      <h1>EM DESENVOLVIMENTO</h1>
+      <h1>EM DESENVOLVIMENTO!</h1>
       <style>
         color: #2B44E7; text-align: center; font-family: Suez One; font-size:
         21.86px; font-style: normal; font-weight: 400; line-height: 39.999px; /*
